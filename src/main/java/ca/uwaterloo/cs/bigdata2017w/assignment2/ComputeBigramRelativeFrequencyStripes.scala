a602896f66df43981d7a3e6fa67c33a26b2a2295
lcpalmer <lcpalmer@student.cs.uwaterloo.ca>
2017-01-26 13:17:12 -0500

    Fixed naming A2

--- src/main/java/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyStripes.scala
+++ src/main/java/ca/uwaterloo/cs/bigdata2017w/assignment2/ComputeBigramRelativeFrequencyStripes.scala
@@ -24,7 +24,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.rogach.scallop._
 
-class StripesTestConf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+class StripesConf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
   mainOptions = Seq(input, output, reducers)
   val input = opt[String](descr = "input path", required = true)
   val output = opt[String](descr = "output path", required = true)
@@ -32,11 +32,11 @@ class StripesTestConf(args: Seq[String]) extends ScallopConf(args) with Tokenize
   verify()
 }
 
-object ComputeBigramRelativeFrequencyStripesTest extends Tokenizer {
+object ComputeBigramRelativeFrequencyStripes extends Tokenizer {
   val log = Logger.getLogger(getClass().getName())
 
   def main(argv: Array[String]) {
-    val args = new StripesTestConf(argv)
+    val args = new StripesConf(argv)
 
     log.info("Input: " + args.input())
     log.info("Output: " + args.output())
