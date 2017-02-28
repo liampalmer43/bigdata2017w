46d0540da985765a2002b2c07d0844e7bdb4d7d2
lcpalmer <lcpalmer@student.cs.uwaterloo.ca>
2017-02-28 15:43:16 -0500

    A5 Q4 Finalized

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q4.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q4.scala
@@ -7,8 +7,6 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.rogach.scallop._
 
-import scala.collection.mutable.ListBuffer
-
 object Q4 {
   val log = Logger.getLogger(getClass().getName())
 
@@ -69,15 +67,15 @@ object Q4 {
         }).reduceByKey(_ + _)
         .map(pair => (Integer.parseInt(pair._1._1), (pair._1._2, pair._2)))
         .sortByKey(numPartitions = 1)
-        .map(pair => {
+        .collect()
+
+      for (i <- 0 until result.length) {
+          val pair = result(i)
           val nationKey = pair._1
           val nationName = pair._2._1
           val count = pair._2._2
           println(s"($nationKey,$nationName,$count)")
-          pair
-        })
-
-      result.saveAsTextFile(outputName)
+      }
 
     } else if (args.parquet()) {
       val sparkSession = SparkSession.builder.getOrCreate
@@ -86,17 +84,16 @@ object Q4 {
       val customerDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/customer")
       val customerRDD = customerDF.rdd
       val customerMap = sc.broadcast(customerRDD
-        .map(row => (row(0), row(3)))
+        .map(row => (row(0).toString, row(3).toString))
         .collectAsMap)
 
       // n_nationkey -> n_name
       val nationDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/nation")
       val nationRDD = nationDF.rdd
       val nationMap = sc.broadcast(nationRDD
-        .map(row => (row(0), row(1)))
+        .map(row => (row(0).toString, row(1).toString))
         .collectAsMap)
 
-      val sparkSession = SparkSession.builder.getOrCreate
       val lineitemDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/lineitem")
       val lineitemRDD = lineitemDF.rdd
       val lineitemData = lineitemRDD
@@ -124,15 +121,15 @@ object Q4 {
         }).reduceByKey(_ + _)
         .map(pair => (Integer.parseInt(pair._1._1), (pair._1._2, pair._2)))
         .sortByKey(numPartitions = 1)
-        .map(pair => {
+        .collect()
+
+      for (i <- 0 until result.length) {
+          val pair = result(i)
           val nationKey = pair._1
           val nationName = pair._2._1
           val count = pair._2._2
           println(s"($nationKey,$nationName,$count)")
-          pair
-        })
-
-      result.saveAsTextFile(outputName)
+      }
 
     } else {
       throw new IllegalArgumentException("Must include at least one of --text or --parquet command line options")
