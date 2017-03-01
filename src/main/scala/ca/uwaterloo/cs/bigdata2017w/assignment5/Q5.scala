e9dac0c8cff661645042ebdbfba83f570719b6a8
lcpalmer <lcpalmer@student.cs.uwaterloo.ca>
2017-02-28 21:45:36 -0500

    Q5 Finalized

--- src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q5.scala
+++ src/main/scala/ca/uwaterloo/cs/bigdata2017w/assignment5/Q5.scala
@@ -9,7 +9,7 @@ import org.rogach.scallop._
 
 import scala.collection.mutable.ListBuffer
 
-class A5PlainConf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+class A5PlainConf(args: Seq[String]) extends ScallopConf(args) {
   mainOptions = Seq(input, reducers)
   val input = opt[String](descr = "input path", required = true)
   val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
@@ -53,7 +53,6 @@ object Q5 {
       val orders = sc.textFile(args.input() + "/orders.tbl")
 
       val lineitemData = lineitems
-        .filter(line => line.split('|')(10).substring(0, date.value.length()) == date.value)
         .map(line => (line.split('|')(0), line.split('|')(10).substring(0, 7)))
 
       val orderData = orders
@@ -80,19 +79,80 @@ object Q5 {
           }
           result.toList
         }).reduceByKey(_ + _)
-        .filter(pair => pair._1._1 == "Canada" || pair._1._1 == "USA")
+        .filter(pair => pair._1._1 == "CANADA" || pair._1._1 == "UNITED STATES")
         .sortByKey(numPartitions = 1)
-        .map(pair => {
+        .collect()
+
+      for (i <- 0 until result.length) {
+          val pair = result(i)
           val nationName = pair._1._1
           val date = pair._1._2
-          val count = pair._2._2
+          val count = pair._2
           println(s"($nationName,$date,$count)")
-          pair
-        })
-
-      result.saveAsTextFile(outputName)
+      }
 
     } else if (args.parquet()) {
+      val sparkSession = SparkSession.builder.getOrCreate
+
+      // c_custkey -> c_nationkey
+      val customerDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/customer")
+      val customerRDD = customerDF.rdd
+      val customerMap = sc.broadcast(customerRDD
+        .map(row => (row(0).toString, row(3).toString))
+        .collectAsMap)
+
+      // n_nationkey -> n_name
+      val nationDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/nation")
+      val nationRDD = nationDF.rdd
+      val nationMap = sc.broadcast(nationRDD
+        .map(row => (row(0).toString, row(1).toString))
+        .collectAsMap)
+
+      val lineitemDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/lineitem")
+      val lineitemRDD = lineitemDF.rdd
+
+      val orderDF = sparkSession.read.parquet("TPC-H-0.1-PARQUET/orders")
+      val orderRDD = orderDF.rdd
+
+      val lineitemData = lineitemRDD
+        .map(row => (row(0).toString, row(10).toString.substring(0, 7)))
+
+      val orderData = orderRDD
+        .map(row => (row(0).toString, row(1).toString))
+
+      val result = lineitemData.cogroup(orderData)
+        .filter(keyIterablePair => keyIterablePair._2._1.iterator.hasNext)
+        .flatMap(keyIterablePair => {
+          // We have (orderKey, (Iter["date"], Iter[custKey]))
+          if (keyIterablePair._2._2.iterator.length != 1) {
+            throw new IllegalArgumentException("Only one valid custKey for an order in the order table")
+          }
+          val custKey = keyIterablePair._2._2.iterator.next;
+          // c_custkey -> c_nationkey
+          // n_nationkey -> n_name
+          val nationKey = customerMap.value(custKey)
+          val nationName = nationMap.value(nationKey)
+
+          val iter = keyIterablePair._2._1.iterator
+          var result = new ListBuffer[((String, String), Int)]()
+          while (iter.hasNext) {
+            val e = ((nationName, iter.next), 1)
+            result += e
+          }
+          result.toList
+        }).reduceByKey(_ + _)
+        .filter(pair => pair._1._1 == "CANADA" || pair._1._1 == "UNITED STATES")
+        .sortByKey(numPartitions = 1)
+        .collect()
+
+      for (i <- 0 until result.length) {
+          val pair = result(i)
+          val nationName = pair._1._1
+          val date = pair._1._2
+          val count = pair._2
+          println(s"($nationName,$date,$count)")
+      }
+
 
     } else {
       throw new IllegalArgumentException("Must include at least one of --text or --parquet command line options")
